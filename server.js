bf054ce2d6bffbfbc8d398a8a7c4c5ffa050c345
co2-git <francoisrvespa@gmail.com>
2014-08-14 17:25:24 +0200

    domain binding

--- server.js
+++ server.js
@@ -15,78 +15,90 @@ if ( ! process.send ) {
   process.send = console.log;
 }
 
-/* ========	start express app	======== */
+var domain = require('domain').create();
 
-var express = require('express');
-var app = express();
-
-/* ======== middlewares  ======== */
-
-var cookieParser = require('cookie-parser');
-
-/* ========	app config	======== */
-
-var config = {
-  'view engine'   :   'jade',
-  'views'         :   'views',
-  'port'          :   3012
-};
-
-for ( var middleware in config ) {
-  app.set(middleware, config[middleware]);
-}
-
-/* ======== static router  ======== */
-
-app.use(express.static(require('path').join(__dirname, 'public')));
-
-/* ======== cookies & session  ======== */
-
-var secret = [process.pid, Math.random(), +new Date()].join();
-
-app.use(cookieParser(secret));
-
-/* ======== response locals  ======== */
-
-app.use(function (req, res, next) {
-  res.locals.req = req;
-  next();
-});
-
-/* ======== home page and results page  ======== */
-
-app.get('/',
-
-  require('./routes/search').bind(app),
-
-  require('./routes/home')
-  );
-
-/* ======== error  ======== */
-
-app.use(function (error, req, res, next) {
-  res.send({
-    error: {
-      message: error.message,
-      stack: error.stack.split(/\n/)
-    }
+domain.on('error', function (error) {
+  console.log('error', {
+    name: error.name,
+    message: error.message,
+    stack: error.stack.split(/\n/)
   });
 });
 
-/* ======== not found  ======== */
+domain.run(function () {
+  /* ======== start express app ======== */
 
-app.use(function (req, res, next) {
-  res.send('Page not found');
-});
+  var express = require('express');
+  var app = express();
 
-/* ======== start server  ======== */
+  /* ======== middlewares  ======== */
 
-var server = require('http').createServer(app);
+  var cookieParser = require('cookie-parser');
 
-server.listen(app.get('port'), function () {
-  process.send({ message: 'started', port: app.get('port'), pid: process.pid });
-});
+  /* ======== app config  ======== */
 
-server.on('error', function (error) {
-  process.send({ 'server error': error });
+  var config = {
+    'view engine'   :   'jade',
+    'views'         :   'views',
+    'port'          :   3012
+  };
+
+  for ( var middleware in config ) {
+    app.set(middleware, config[middleware]);
+  }
+
+  /* ======== static router  ======== */
+
+  app.use(express.static(require('path').join(__dirname, 'public')));
+
+  /* ======== cookies & session  ======== */
+
+  var secret = [process.pid, Math.random(), +new Date()].join();
+
+  app.use(cookieParser(secret));
+
+  /* ======== response locals  ======== */
+
+  app.use(function (req, res, next) {
+    res.locals.req = req;
+    next();
+  });
+
+  /* ======== home page and results page  ======== */
+
+  app.get('/',
+
+    require('./routes/search').bind(app),
+
+    require('./routes/home')
+    );
+
+  /* ======== error  ======== */
+
+  app.use(function (error, req, res, next) {
+    res.send({
+      error: {
+        message: error.message,
+        stack: error.stack.split(/\n/)
+      }
+    });
+  });
+
+  /* ======== not found  ======== */
+
+  app.use(function (req, res, next) {
+    res.send('Page not found');
+  });
+
+  /* ======== start server  ======== */
+
+  var server = require('http').createServer(app);
+
+  server.listen(app.get('port'), function () {
+    process.send({ message: 'started', port: app.get('port'), pid: process.pid });
+  });
+
+  server.on('error', function (error) {
+    process.send({ 'server error': error });
+  });
 });
